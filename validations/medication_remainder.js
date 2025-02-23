315eb45664c943c9e7c78cea2abbd2d81387e6d3
ayom04 <abdullahayomide04@gmail.com>
2025-02-23 08:36:21 +0100

    feat(reduced the time span)

--- validations/medication_remainder.js
+++ validations/medication_remainder.js
@@ -4,7 +4,7 @@ const medicationRemainderSchema = Joi.object({
   medication_name: Joi.string().required(),
   medication_dose: Joi.string().required(),
   medication_time: Joi.string(),
-  repeat_interval: Joi.number().integer().required(),
+  repeat_interval: Joi.number().required(),
   last_sent: Joi.date(),
   medication_date: Joi.date(),
   schedule: Joi.date(),
